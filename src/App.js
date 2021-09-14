8f6ec76fca1b822c41ebbee85f78ec8e2097e597
Mikko Lainas <n0lami00@students.oamk.fi>
2021-09-14 12:55:33 +0300

    valmis esimerkki

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import FillOptions from './FillOptions';
 
 function App() {
+  const [brutto, setBrutto] = useState('');
+  const [vero, setVero] = useState('');
+  const [veroSumma, setVerosumma] = useState(0);
+  const [tv, setTv] = useState(1);
+  const [tyottomyysvakuutusSumma, setTyottomyysvakuutusSumma] = useState(0);
+  const [te, setTe] = useState(1);
+  const [tyoelakeSumma, SetTyoelakeSumma] = useState(0);
+  const [netto, setNetto] = useState(0);
+
+  function calculate(e) {
+    e.preventDefault();
+    const veroEurot = brutto / 100 * vero;
+    const teEurot = brutto / 100 * te;
+    const tvEurot = brutto / 100 * tv;
+    setVerosumma(veroEurot);
+    SetTyoelakeSumma(teEurot);
+    setTyottomyysvakuutusSumma(tvEurot);
+    setNetto(brutto - veroEurot - teEurot - tvEurot);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div id="container">
+      <h3>Palkkalaskuri</h3>
+      <form onSubmit={calculate}>
+        <div>
+          <label>Brutto</label>
+        </div>
+        <input type="number"
+          value={brutto}
+          onChange={e => setBrutto(e.target.value)}
+        />
+        <div>
+          <label>Vero %</label>
+        </div>
+        <input type="number"
+          value={vero}
+          onChange={e => setVero(e.target.value)}
+        />
+        <div>
+          <label>Työttömyysvakuutus (%)</label>
+          <output>{tyottomyysvakuutusSumma}</output>
+        </div>
+        <select
+          value={tv}
+          onChange={e => setTv(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+         <FillOptions />
+        </select>
+        <div>
+          <label>Työeläke (%)</label>
+          <output>{tyoelakeSumma}</output>
+        </div>
+        <select
+          value={te}
+          onChange={e => setTe(e.target.value)}
+        >
+         <FillOptions />
+        </select>
+        <div>
+          <label>Nettopalkkasi </label>
+          <output id="netto">{netto}</output>
+
+        </div>
+        <button>Laske</button>
+      </form>
     </div>
   );
 }
